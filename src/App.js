94ed7e545c35f43ac0ace431b373d99b563be345
Santiago Giraldo <69825324+sgiraldo2@users.noreply.github.com>
2023-05-13 00:10:24 -0500

    team todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+import Todo from './components/Todo';
+
+import { db } from './firebase';
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+} from 'firebase/firestore';
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#242424] to-[#323232]`,
+  container: `bg-slate-100 max-w-[500px] mx-auto p-4 rounded-md shadow-xl`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-blue-500 text-slate-100`,
+  count: `text-center text-center`,
+};
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  // create todo
+  const createTodo = async (e) => {
+    e.preventDefault();
+    if (input === '') {
+      alert('Please enter a todo');
+      return;
+    }
+    await addDoc(collection(db, 'todos'), {
+      text: input,
+      completed: false,
+    });
+    setInput('');
+  };
+
+  // read todo from firebase
+  useEffect(() => {
+    const q = query(collection(db, 'todos'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todoArr = [];
+      querySnapshot.forEach((doc) => {
+        todoArr.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todoArr);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // update todofrom firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todos', todo.id), {
+      completed: !todo.completed,
+    });
+  };
+
+  // delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos', id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Team Todo List</h3>
+        <form onSubmit={createTodo} className={style.form}>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={style.input}
+            type="text"
+            placeholder="Add Item"
+          />
+          <button className={style.button}>
+            <AiOutlinePlus size={30}></AiOutlinePlus>
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+            />
+          ))}
+        </ul>
+        {todos.length === 0 ? null : (
+          <p className={style.count}>{`You have ${todos.length} todos`}</p>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaRegTrashAlt } from 'react-icons/fa';
+
+const style = {
+  listItem: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  listItemComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize line-through`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer flex items-center`,
+};
+
+const Todo = ({ todo, toggleComplete, deleteTodo }) => {
+  return (
+    <li className={todo.completed ? style.listItemComplete : style.listItem}>
+      <div className={style.row}>
+        <input
+          onChange={() => toggleComplete(todo)}
+          type="checkbox"
+          checked={todo.completed ? 'checked' : ''}
+        />
+        <p
+          onClick={() => toggleComplete(todo)}
+          className={todo.completed ? style.textComplete : style.text}
+        >
+          {todo.text}
+        </p>
+      </div>
+      <button onClick={() => deleteTodo(todo.id)} className={style.button}>
+        {' '}
+        {<FaRegTrashAlt size={20} />}{' '}
+      </button>
+    </li>
+  );
+};
+
+export default Todo;
